8e19bb159df8af8bb8769c4d3760a6823b42439e
sebboyer <sebboyer@mit.edu>
2015-05-31 09:13:12 -0700

    exp 7

--- Main_Adaptive_clustering.py
+++ Main_Adaptive_clustering.py
@@ -6,7 +6,8 @@ import read_experiment_table as ret
 
 exp_file="experiment/exp_parameters.csv"
 #exp_list=['a','b','c','d','e','f','g']+['a1','b1','c1','d1','e1','f1','g1']+['a0','b0','c0','d0','e0','f0','g0']
-exp_list=['a6','b6','c6','d6','e6','f6','g6']
+
+exp_list=['a7','b7','c7','d7','e7','f7']
 ret.conduct_experiment(exp_file,exp_list)
 
 
